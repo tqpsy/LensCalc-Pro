e8dcf927bb5776d0f4e39b4fa3e3902d14f6d25f
tqpsy <luca.topsidakis@gmail.com>
2026-09-08 17:19:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'ottica-v1';
-const urlsToCache = ['index.html', 'manifest.json'];
+const urlsToCache = ['index.html', 'manifest.json', 'icon.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
